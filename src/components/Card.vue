a632c3635ce76d85d94127a6447986f84da8f559
Jules <j.bousrez@outlook.com>
2024-01-24 14:15:20 +0100

    feat: added a delete btn with a comfirmation popup

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,60 +1,123 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, ref } from "vue";
+import Modal from "../components/Modal.vue";
 
-defineProps({
-    title: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    },
-    type: {
-        type: String,
-        required: true,
-    },
-    id: {
-        type: Number,
-        required: true,
-    },
-})
+const isModalVisible = ref(false);
 
+const showModal = () => {
+	isModalVisible.value = true;
+};
+
+const closeModal = () => {
+	isModalVisible.value = false;
+};
+
+const props = defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+		required: true,
+	},
+	type: {
+		type: String,
+		required: true,
+	},
+	id: {
+		type: Number,
+		required: true,
+	},
+});
+
+const handleModal = () => {
+	showModal();
+};
+
+const handleDelete = async () => {
+	const usertoken = localStorage.getItem("token");
+
+	if (!usertoken) {
+		return (window.location.href = "/login");
+	}
+
+	const response = await fetch(`http://127.0.0.1:8000/api/${props.type}/${props.id}`, {
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+			Authorization: "Bearer " + usertoken,
+		},
+		method: "DELETE",
+	});
+
+	if (response.ok) {
+		closeModal();
+		window.location.reload();
+	} else if (response.status === 401) {
+		localStorage.removeItem("token");
+		window.location.href = "/login";
+	} else {
+		throw "Error while deleting";
+	}
+};
 </script>
 
 <template>
-    <router-link :to="{ path: '/' + type + '/' + id }">
-        <div class="column">
-            <img :src="image" :alt="title">
-            <p>{{ title }}</p>
-        </div>
-    </router-link>
+	<Modal v-show="isModalVisible" @close="closeModal" @handleDelete="handleDelete">
+		<template v-slot:body>
+			<p>Do you really want to delete {{ title }}?</p>
+		</template>
+	</Modal>
+
+	<router-link :to="{ path: '/' + type + '/' + id }">
+		<div class="column">
+			<img :src="image" :alt="title" />
+			<p>{{ title }}</p>
+
+			<div class="row">
+				<router-link :to="{ path: '/' + type + '/' + id + '/edit' }">Edit</router-link>
+				<a @click.prevent="handleModal">Delete</a>
+			</div>
+		</div>
+	</router-link>
 </template>
 
 <style scoped>
 .column {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 1rem;
-    border-radius: 7px;
-    transition: all 0.3s;
-    cursor: pointer;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 1rem;
+	border-radius: 7px;
+	transition: all 0.3s;
+	cursor: pointer;
 }
 
 .column:hover {
-    background-color: #ECECEC;
+	background-color: #ececec;
+}
+
+.row {
+	margin-top: 10px;
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	flex-wrap: wrap;
+	gap: 15px;
+	justify-content: center;
 }
 
 .row img {
-    border-radius: 5px;
-    height: 200px;
-    width: 150px;
+	border-radius: 5px;
+	height: 200px;
+	width: 150px;
 }
 
 .row p {
-    text-align: center;
-    margin-top: 10px;
+	text-align: center;
+	margin-top: 10px;
 }
 </style>
